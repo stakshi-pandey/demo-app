987f33df364c0ee19bf657839546ea1b5325cf7c
stakshi-pandey <stakshi.pandey@studymitr.com>
2023-02-11 20:30:35 +0530

    fix: post model validaation type

--- models/postModel.go
+++ models/postModel.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Post struct {
 	gorm.Model
-	Title string `json:"title" validate:"alpha"`
+	Title string `json:"title" validate:"required"`
 	Body  string `validate:"required"`
 }
